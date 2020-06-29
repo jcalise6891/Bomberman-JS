375a64d379c941dd6cfbad24c572d2d6bd939126
jcalise6891 <calise.j@codeur.online>
2020-06-29 15:09:50 +0200

    J'comprends pas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,9 @@
 let j1 = null;
 
 window.onload=init;
-window.addEventListener("keydown",move(Event));
+window.addEventListener("keydown",function(Event){
 
-function init(){
-    j1 = document.getElementById('player');
-    j1.style.position.left="0px";
-    j1.style.position.top="0px";
-}
-
-function move (key){
-
-    let key_code = key.code;
+    let key_code = Event.keyCode;
 
     console.log('touche '+ key_code);
 
@@ -29,7 +21,36 @@ function move (key){
             moveDown();
             break;
     }
+
+});
+
+function init(){
+    j1 = document.getElementById('player');
+    j1.style.position.left="0px";
+    j1.style.position.top="0px";
 }
+
+// function move (key){
+
+//     let key_code = key.code;
+
+//     console.log('touche '+ key_code);
+
+//     switch(key_code){
+//         case 37: // Gauche
+//             moveLeft();
+//             break;
+//         case 38: // Haut
+//             moveUp();
+//             break;
+//         case 39: // Droit
+//             moveRight();
+//             break;
+//         case 40: // Bas
+//             moveDown();
+//             break;
+//     }
+// }
 
 function moveLeft(){
     j1.style.left =parseInt(j1.style.left)-50+'px';
