cfc6f44d3f0c787699698e45568ab87c423ef078
jcalise6891 <calise.j@codeur.online>
2020-06-30 14:54:13 +0200

    Hey oui

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
-let j1          = null;
-let b           = null;
-let ennemies    = [];
+let j1          = null;                                                                     // Div Joueur
+let ennemies    = [];                                                                       // Tableau contenant les différentes div ennemies
 let footstep    = new Audio("./assets/sound/sfx_footstep.wav");
 let wall        = new Audio('./assets/sound/sfx_wall.wav');
 
@@ -9,7 +8,7 @@ footstep.volume = 0.05;
 
 window.onload=init;
 
-window.addEventListener("keydown",function(Event){
+window.addEventListener("keydown",function(Event){                                          // Détection des touches du clavier pour les différents mouvements du joueur
 
     let key_code = Event.keyCode;
 
@@ -29,12 +28,39 @@ window.addEventListener("keydown",function(Event){
             moveDown(j1);
             break;
     }
-
 });
 
+document.getElementById('start').addEventListener("click", function(){                      // Déplacement Monstre
 
+    this.style.display="none";
+    document.getElementById('overlay').style.display="none";
 
-function init(){
+    let inter = setInterval(() => {
+
+        let mv = rand(1,6,1);
+        let e = rand(0,6,1);
+
+        switch (mv){
+            case 1:
+                moveUp(ennemies[e]);
+                break;
+            case 2:
+                moveDown(ennemies[e]);
+                break;
+            case 3:
+                moveLeft(ennemies[e]);
+                break;
+            case 4:
+                moveRight(ennemies[e]);
+                break;
+        }
+            
+    }, 200);
+
+    }
+);
+
+function init(){                                                                            // Initialise Le plateau de jeu Ainsi que la position des différentes entitées
     j1 = document.getElementById('player');
     j1.style.left="0px";
     j1.style.top="0px";
@@ -50,12 +76,10 @@ function init(){
         ennemies[i].style.top="0px";        
  
         ennemies[i].style.left =parseInt(ennemies[i].style.left)+rand(0,800,50)+'px';
-        ennemies[i].style.top =parseInt(ennemies[i].style.top)+rand(0,800,50)+'px';
-       
+        ennemies[i].style.top =parseInt(ennemies[i].style.top)+rand(0,800,50)+'px';  
     }
-
 }
-
+//========================================================================================= Methode de mouvement des entitées ========================
 function moveLeft(div){
     if(parseInt(div.style.left) > 0){
         div.style.left =parseInt(div.style.left)-50+'px';
@@ -108,7 +132,9 @@ function moveDown(div){
     }
 }
 
-function rand(min, max, step) {
+//================================================================================================================================================
+
+function rand(min, max, step) {                                 // Méthode qui retourne un chiffre aléatoire selon le min, le max et la pas
     let delta,
         range,
         rand;
@@ -129,34 +155,7 @@ function rand(min, max, step) {
     return rand;
 }
 
-document.getElementById('start').addEventListener("click", function(){
 
-    this.style.display="none";
-
-    let inter = setInterval(() => {
-
-        let mv = rand(1,5,1);
-        let e = rand(0,6,1);
-
-        switch (mv){
-            case 1:
-                moveUp(ennemies[e]);
-                break;
-            case 2:
-                moveDown(ennemies[e]);
-                break;
-            case 3:
-                moveLeft(ennemies[e]);
-                break;
-            case 4:
-                moveRight(ennemies[e]);
-                break;
-        }
-            
-    }, 200);
-
-    }
-);
 
 
 
